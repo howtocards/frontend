865098eaf7d9c9533f6ef9bc0a852a299500f2d0
Sergey Sova <mail@sergeysova.com>
2018-05-10 22:52:13 +0300

    feat(ui/organisms): create primitive footer

--- src/ui/organisms/index.js
+++ src/ui/organisms/index.js
@@ -1,1 +1,2 @@
 export { Header } from './header'
+export { PrimitiveFooter } from './primitive-footer'
---
+++ src/ui/organisms/primitive-footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { Link } from 'ui/atoms'
+
+
+const FooterBlock = styled.footer`
+  display: flex;
+  flex-flow: row;
+  border-top: 1px solid ${({ theme }) => theme.palette.decoration.borders};
+  padding: 2rem 0;
+  width: 100%;
+  justify-content: center;
+`
+
+export const PrimitiveFooter = () => (
+  <FooterBlock>
+    Go to&nbsp;<Link to="/">home</Link>&nbsp;page
+  </FooterBlock>
+)
