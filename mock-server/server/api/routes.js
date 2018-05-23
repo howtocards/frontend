8e8b588223c1b30168dd93a9e018503c6f0e9611
Sergey Sova <ssova@yandex-team.ru>
2018-05-23 03:12:23 +0300

    fix(server/api/middlewares): authenticated should check token for exists

--- mock-server/server/api/routes.js
+++ mock-server/server/api/routes.js
@@ -22,7 +22,7 @@ const authenticated = () => async (ctx, next) => {
   if (ctx.request.header.authorization) {
     const [word, token] = ctx.request.header.authorization.split(' ')
 
-    if (word === 'token' && token.length > 10) {
+    if (word === 'token' && token && token.length > 10) {
       const result = (await userGet(token))
         .map((user) => {
           ctx.user = user
