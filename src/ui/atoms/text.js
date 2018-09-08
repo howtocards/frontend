150aab73ff441312e1dd92f06c0a2d2d4d7f693d
Abu Shamsutdinov <yakotikadot@gmail.com>
2018-09-08 22:14:45 +0300

    refactor(ui/atoms): fix eslint

--- src/ui/atoms/text.js
+++ src/ui/atoms/text.js
@@ -2,6 +2,6 @@ import styled from 'styled-components'
 
 
 export const Text = styled.p`
-    color: ${({ theme }) => theme.palette.primary.initial.background};
-    font-size: ${({ size }) => size || '1.4rem'};
+  color: ${({ theme }) => theme.palette.primary.initial.background};
+  font-size: ${({ size }) => size || '1.4rem'};
 `
