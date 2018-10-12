b621ff1e52aa30bb6cc0dda6d872f09b8160f786
Sergey Sova <mail@sergeysova.com>
2018-10-13 00:11:33 +0300

    feat(ui/atoms): add narrow property to heading

--- src/ui/atoms/heading.js
+++ src/ui/atoms/heading.js
@@ -1,17 +1,28 @@
+/* eslint-disable no-magic-numbers */
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
 
 export const H1 = styled.h1`
-  margin: 3rem 0;
+  margin: ${(p) => p.narrow ? 0 : 3}rem 0;
   user-select: none;
 `
+H1.propTypes = {
+  narrow: PropTypes.bool,
+}
 
 export const H2 = styled.h2`
-  margin: 2rem 0;
+  margin: ${(p) => p.narrow ? 0 : 2}rem 0;
   user-select: none;
 `
+H2.propTypes = {
+  narrow: PropTypes.bool,
+}
 
 export const H3 = styled.h3`
-  margin: 2rem 0;
+  margin: ${(p) => p.narrow ? 0 : 2}rem 0;
   user-select: none;
 `
+H3.propTypes = {
+  narrow: PropTypes.bool,
+}
