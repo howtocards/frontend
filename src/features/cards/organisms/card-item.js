63b03bf532ab366c2ebd2f9e46c53f0f25f2c485
Sergey Sova <mail@sergeysova.com>
2018-10-13 03:01:06 +0300

    feat(features/cards): add code highlighting

--- src/features/cards/organisms/card-item.js
+++ src/features/cards/organisms/card-item.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import { format } from 'date-fns'
 import Markdown from 'react-markdown'
+import highlight from 'highlightjs'
 
 import { Col, Row } from 'styled-components-layout'
 import { Card, H3, Text } from 'ui/atoms'
@@ -12,21 +13,47 @@ const CardBox = styled.div`
   margin: 1.5rem 0.5rem;
 `
 
-export const CardItem = ({ title, created_at: createdAt, content }) => (
-  <CardBox>
-    <Card>
-      <Col>
-        <Row justify="space-between">
-          <H3 narrow>{title}</H3>
-          <i narrow>{format(new Date(createdAt), 'HH:MM MM/DD/YYYY')}</i>
-        </Row>
-      </Col>
-      <Text>
-        <Markdown source={content} />
-      </Text>
-    </Card>
-  </CardBox>
-)
+export class CardItem extends Component {
+  contentRef = React.createRef()
+
+  highlightBlocks() {
+    if (this.contentRef.current) {
+      const list = [...this.contentRef.current.querySelectorAll('code').values()]
+
+      list
+        .filter((node) => node.className.indexOf('language') > -1)
+        .forEach((node) => highlight.highlightBlock(node))
+    }
+  }
+
+  componentDidMount() {
+    this.highlightBlocks()
+  }
+
+  componentDidUpdate() {
+    this.highlightBlocks()
+  }
+
+  render() {
+    const { title, created_at: createdAt, content } = this.props
+
+    return (
+      <CardBox>
+        <Card>
+          <Col>
+            <Row justify="space-between">
+              <H3 narrow>{title}</H3>
+              <i>{format(new Date(createdAt), 'HH:MM MM/DD/YYYY')}</i>
+            </Row>
+          </Col>
+          <Text innerRef={this.contentRef}>
+            <Markdown source={content} />
+          </Text>
+        </Card>
+      </CardBox>
+    )
+  }
+}
 
 CardItem.propTypes = {
   title: PropTypes.string.isRequired,
