924aea6c72696cdf02ffa8b075dc61a1ded0a829
Sergey Sova <mail@sergeysova.com>
2018-10-19 21:56:54 +0300

    feat(ui/atoms): add ErrorBox

---
+++ src/ui/atoms/error-box.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+
+export const ErrorBox = styled.div`
+  display: block;
+  border: 1px solid red;
+  padding: 0.4rem 0.6rem;
+  color: red;
+  border-radius: 0.4rem;
+`
