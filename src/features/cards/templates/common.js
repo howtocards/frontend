8b8d3cc5596895b9799e7cf56b6efb687b403a82
Sergey Sova <mail@sergeysova.com>
2018-10-27 21:45:44 +0300

    feat(features/cards): common cards template should use sidebar template
    CARDS-69

--- src/features/cards/templates/common.js
+++ src/features/cards/templates/common.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 import { CommonContentTemplate } from 'features/common'
-import { Container } from 'ui/templates'
+import { Container, SidebarTemplate } from 'ui/templates'
+import { Card } from 'ui/atoms'
 
+
+const StickyCard = styled(Card)`
+  position: sticky;
+  top: 2rem;
+`
+
+const Sidebar = () => (
+  <StickyCard>
+    Sidebar placeholder
+  </StickyCard>
+)
 
 export const CardsCommonTemplate = ({ children }) => (
   <CommonContentTemplate>
     <Container>
-      {children}
+      <SidebarTemplate sidebar={<Sidebar />}>
+        {children}
+      </SidebarTemplate>
     </Container>
   </CommonContentTemplate>
 )
