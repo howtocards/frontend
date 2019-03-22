94ab5113286a9559adb7ec7c66d464b2c87a1200
Natalia <natalia.averin@gmail.com>
2019-03-22 19:07:44 +0100

    feat(features/cards): added popup into card-item
    added a bit redesigned with grids popup into card-item

--- src/features/cards/organisms/card-item.js
+++ src/features/cards/organisms/card-item.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import styled, { css } from "styled-components"
+import styled from "styled-components"
 import PropTypes from "prop-types"
 import { format } from "date-fns"
 
