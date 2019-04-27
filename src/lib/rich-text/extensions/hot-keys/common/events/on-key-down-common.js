68452949d10ec8cef6c179a14840d504a4b62283
Sergey Sova <mail@sergeysova.com>
2019-04-27 16:24:53 +0300

    fix(lib/rich-text): dependency cycle

--- src/lib/rich-text/extensions/hot-keys/common/events/on-key-down-common.js
+++ src/lib/rich-text/extensions/hot-keys/common/events/on-key-down-common.js
@@ -1,5 +1,9 @@
 import { isModEnter, isBackspace, isEnter, isModA, isTab } from "../../constant"
-import { onEnter, onBackspace, onModEnter, onSelectAll, onTab } from "."
+import { onBackspace } from "./on-backspace"
+import { onModEnter } from "./on-mod-enter"
+import { onEnter } from "./on-enter"
+import { onSelectAll } from "./on-select-all"
+import { onTab } from "./on-tab"
 
 export const onKeyDownCommon = ({ opts, event, change, editor }) => {
   const args = { opts, event, change, editor }
