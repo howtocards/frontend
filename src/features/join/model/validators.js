3046ed526e0f5a3522a1c3877a70a5a004576292
Sergey Sova <mail@sergeysova.com>
2019-05-02 14:33:00 +0300

    fix(features/join): use simple email validator

--- src/features/join/model/validators.js
+++ src/features/join/model/validators.js
@@ -1,4 +1,5 @@
-const emailRegexp = /.{2,}@(?!:\/\/)([a-zA-Z0-9-_]+\.)*[a-zA-Z0-9][a-zA-Z0-9-_]+\.[a-zA-Z]{2,11}?$/gim
+// const emailRegexp = /.{2,}@(?!:\/\/)([a-zA-Z0-9-_]+\.)*[a-zA-Z0-9][a-zA-Z0-9-_]+\.[a-zA-Z]{2,11}?$/gim
+const emailRegexp = /.{2,}@.{2,}/
 export const emailValidator = (value) => {
   if (value < 1) return "Please, enter email"
   if (!emailRegexp.test(value)) return "Please, enter correct email"
