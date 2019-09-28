b1bc80039ba029ba82d7815f36933a137f219cc7
Sergey Sova <mail@sergeysova.com>
2019-09-28 13:57:51 +0300

    feat(features/settings): change gravatar email to set avatar
    HOW-62

--- src/pages/settings/page.js
+++ src/pages/settings/page.js
@@ -3,25 +3,32 @@ import * as React from "react"
 import styled from "styled-components"
 import { useStore } from "effector-react"
 
+import { Col, Row } from "@lib/styled-components-layout"
 import { SettingsTemplate } from "@features/settings"
-import { Button, H4, Input } from "@howtocards/ui"
+import { Button, ButtonPrimary, H4, Input, ZeroButton } from "@howtocards/ui"
 import {
+  $avaEmail,
+  $avaEmailChanged,
+  $avatarUrl,
   $isDisabled,
   $name,
   $nameChanged,
+  gravatarChangeCancelled,
+  gravatarEmailChanged,
+  gravatarEmailSubmitted,
   nameChanged,
   nameSubmitted,
   pageMounted,
   pageUnmounted,
-} from "./store"
+} from "./model"
 
 export const SettingsPage = () => {
   React.useEffect(() => (pageMounted(), pageUnmounted))
 
   return (
     <SettingsTemplate title="Personal settings">
-      <NameSection />
       <AvatarSection />
+      <NameSection />
     </SettingsTemplate>
   )
 }
@@ -42,27 +49,112 @@ const NameSection = () => {
   return (
     <FormSection title="Display name">
       <form onSubmit={submit}>
-        <Input
-          disabled={isDisabled}
-          label="Enter your name and press Enter"
-          value={name}
-          onChange={nameChanged}
-        />
-        <Button disabled={buttonDisabled} type="submit">
-          Save
-        </Button>
+        <Col gap="1rem">
+          <Input
+            disabled={isDisabled}
+            label="Enter your name and press Enter"
+            value={name}
+            onChange={nameChanged}
+          />
+          <Row>
+            <Button disabled={buttonDisabled} type="submit">
+              Save
+            </Button>
+          </Row>
+        </Col>
       </form>
     </FormSection>
   )
 }
 
 const AvatarSection = () => {
+  const avatarUrl = useStore($avatarUrl)
+
   return (
     <FormSection title="Avatar">
-      Avatar changing form with some logic
+      <AvatarTemplate>
+        <Avatar width={140} height={140} src={avatarUrl} />
+        <ChangeAvatar />
+      </AvatarTemplate>
     </FormSection>
   )
 }
+
+const ChangeAvatar = () => {
+  const [opened, setOpened] = React.useState(false)
+  const gravatarEmail = useStore($avaEmail)
+  const isDisabled = useStore($isDisabled)
+  const isChanged = useStore($avaEmailChanged)
+
+  const isSaveDisabled = isDisabled || !isChanged
+
+  const cancel = React.useCallback(
+    (event) => {
+      setOpened(false)
+      gravatarChangeCancelled(event)
+    },
+    [setOpened],
+  )
+
+  const save = React.useCallback((event) => {
+    event.preventDefault()
+    gravatarEmailSubmitted(event)
+    setOpened(false)
+  }, [])
+
+  if (opened) {
+    return (
+      <form onSubmit={save} disabled={isSaveDisabled}>
+        <Col gap="1rem">
+          <Input
+            disabled={isDisabled}
+            label="Enter Email for Gravatar"
+            value={gravatarEmail}
+            onChange={gravatarEmailChanged}
+          />
+          <Row gap="1rem">
+            <ButtonPrimary disabled={isSaveDisabled} type="submit">
+              {gravatarEmail.trim().length === 0 ? "Use default" : "Save"}
+            </ButtonPrimary>
+            <ZeroButton onClick={cancel}>Cancel</ZeroButton>
+          </Row>
+        </Col>
+      </form>
+    )
+  }
+
+  return (
+    <Col gap="1rem">
+      <span>
+        Gravatar email: <b>{gravatarEmail}</b>
+      </span>
+      <Row>
+        <Button disabled={isDisabled} onClick={() => setOpened(true)}>
+          Change
+        </Button>
+      </Row>
+    </Col>
+  )
+}
+
+const Avatar = styled.img`
+  display: block;
+  border-radius: 3px;
+  border: 1px solid lightgray;
+`
+
+const AvatarTemplate = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+
+  ${Avatar} {
+    margin-right: 1rem;
+  }
+
+  form {
+    flex-grow: 1;
+  }
+`
 
 type FormSectionProps = {
   title: string,
