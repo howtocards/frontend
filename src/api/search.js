22a7b9a74463ec265a98a326ebf4fc8f46a4666b
Sergey Sova <mail@sergeysova.com>
2019-10-03 21:39:33 +0300

    refactor(features/search): move to api

--- src/api/search.js
+++ src/api/search.js
@@ -1,6 +1,5 @@
 // @flow
-import { request } from "@features/common"
-import type { Card } from "@features/common"
+import { type Card, request } from "@features/common"
 
 const search = (query: string): Promise<{ cards: Card[] }> =>
   request("GET", `/search/?q=${query}`)
