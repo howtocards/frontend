e202b4587719134a88647e71f7e3f7e9904ac8e2
Sergey Sova <mail@sergeysova.com>
2019-10-25 09:40:24 +0300

    fix(app): layout padding on mobiles
    HOW-78

--- src/ui/templates/container.js
+++ src/ui/templates/container.js
@@ -10,4 +10,8 @@ export const Container = styled(WithTag)`
   flex: inherit;
   flex-flow: inherit;
   flex-direction: inherit;
+
+  @media screen and (max-width: 120rem) {
+    padding: 0 1rem;
+  }
 `
